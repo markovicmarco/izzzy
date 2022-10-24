64646edc96410fee8add06bf1b82802082ef3f63
Marko Markovic <archorrs@gmail.com>
2022-10-24 09:40:51 +0200

    Add files via upload

---
+++ utils/counter-util.tsx
@@ -0,0 +1,27 @@
+export type TimeLeft = {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  };
+  
+  export const calculateTimeLeft = () => {
+    let year = new Date('2022').getFullYear();
+    let month = new Date('10').getMonth() + 1;
+    let day = new Date('00').getDate() + 2;
+  
+    const difference = +new Date(`${year}-${month}-${day}`) - +new Date();
+  
+    let timeLeft: TimeLeft = {} as TimeLeft;
+  
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+  
+    return timeLeft;
+  };
